3b958a2177aa2f20f39008af7751ed4bc6ab1d83
muxtorjonov2005 <muxtorjonoveldiyor@gmail.com>
2024-06-18 12:19:44 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,239 @@
+// 1-masala
+// for (let i = 1; i <= 50; i++) {
+//   if (i == 30) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+// 2-masala
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 5 == 0) {
+//         continue
+//     }
+//     console.log(i);
+// }
+
+// 3-masala
+// for (let i = 1; i <= 100; i++) {
+//     if ( i == 75) {
+//         break
+//     }
+//     console.log(i);
+// }
+
+// 4-masala
+// for (let i = 1; i <= 30; i++) {
+//     if (i % 3 == 0) {
+//         continue
+//     }
+//     console.log(i);
+// }
+
+// 5-masala
+// let i = 1
+// while (i <= 20) {
+//     console.log(i);
+//     i++;
+// }
+
+// 6-masala
+// let i = 10
+// while (i >= 1){
+//     console.log(i);
+//     i--
+// }
+
+// 7-masala
+// let i = 1
+// while (i <= 50) {
+//     if (i % 2 == 0){
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+// 8-masala
+// let i = 1
+// while (i <= 50) {
+//     if (i % 2 == 1) {
+//         console.log(i);
+//     }
+//     i++
+// }
+
+// 9-masala
+// let i = 1
+// let sum = 0
+// while (i <= 100){
+//     sum +=i
+//     i++
+// }
+// console.log(sum);
+
+// 10-masala
+// let i = 1
+// while (i <=10){
+//     console.log(i * i);
+//     i++
+// }
+
+// 11-masala
+
+// 12-masala
+// let i = 1
+// while  (i <= 1024){
+//     console.log(i);
+//     i *= 2
+// }
+
+// 13-masala
+
+// 14-masala
+// let i = 1
+// while (i <= 20) {
+//     if (i % 3 == 0){
+//         console.log(i);
+//     }
+//     i++
+// }
+
+// 15-masala
+// let i = 1
+// do {
+//     console.log(i);
+//     i++
+// }while(i <= 10)
+
+// 16-masala
+// let i = 20
+// do {
+//     console.log(i);
+//     i--
+// } while(i >= 1)
+
+// 17-masala
+// let i = 1
+// do {
+//     if (i % 2 == 0){
+//         console.log(i);
+//     }
+//     i++
+// } while (i <= 100)
+
+// 18-masala
+
+// 19-masala
+
+// 20-masala
+// for (let i = 1; i <= 50; i++){
+//     if (i % 5 == 0 && i % 7 == 0){
+//         console.log(`${i} beshga va yettiga karrali`);
+//     } else if (i % 7 == 0){
+//         console.log(`${i} yettiga karrali`);
+//     } else if (i % 5 == 0){
+//         console.log(`${i} beshga karrali`);
+//     }
+// }
+
+// 21-masala
+// for (let i = 1; i <= 12; i++) {
+//   switch (i) {
+//     case 1:
+//       console.log("1 - Yanvar");
+//       break;
+//     case 2:
+//       console.log("2 - Fevral");
+//       break;
+//     case 3:
+//       console.log("3 - Mart");
+//       break;
+//     case 4:
+//       console.log("4 - Aprel");
+//       break;
+//     case 5:
+//       console.log("5 - May");
+//       break;
+//     case 6:
+//       console.log("6 - Iyun");
+//       break;
+//     case 7:
+//       console.log("7 - Iyul");
+//       break;
+//     case 8:
+//       console.log("8 - Avgust");
+//       break;
+//     case 9:
+//       console.log("9 - Sentabr");
+//       break;
+//     case 10:
+//       console.log("10 - Oktabr");
+//       break;
+//     case 11:
+//       console.log("11 - Noyabr");
+//       break;
+//     case 12:
+//       console.log("12 - Dekabr");
+//       break;
+//   }
+// }
+
+// 22-masala
+// let i = 1
+// while (i <= 100){
+//     if (i == 77){
+//         break;
+//     }
+//     if (i % 2 == 0){
+//         i++
+//         continue;
+//     }
+//     console.log(i);
+//     i++
+// }
+
+// 23-masala
+// let j = 1;
+// while (j <= 7) {
+//   switch (j) {
+//     case 1:
+//       console.log("1 - Dushanba");
+//       break;
+//     case 2:
+//       console.log("2 - Seshanba");
+//       break;
+//     case 3:
+//       console.log("3 - Chorshanba");
+//       break;
+//     case 4:
+//       console.log("4 - Payshanba");
+//       break;
+//     case 5:
+//       console.log("5 - Juma");
+//       break;
+//     case 6:
+//       console.log("6 - Shanba");
+//       break;
+//     case 7:
+//       console.log("7 - Yakshanba");
+//       break;
+//   }
+//   j++;
+// }
+
+// 24-masala
+// for (i = 1; i <= 100; i++){
+//     if (i % 3 == 0 && i % 5 == 0){
+//         console.log('FizzBuzz');
+//     } else if (i % 3 == 0){
+//         console.log('Fizz');
+//     } else if (i % 5 == 0){
+//         console.log('Buzz');
+//     }
+// }
+
+// 25-masala
+
+// 26-masala
+
+// 27-masala
